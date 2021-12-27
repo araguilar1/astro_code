96e35c4a32e1a6a02e80fdcd701222025c236db2
araguilar1 <araguilar@crimson.ua.edu>
2021-12-26 18:13:28 -0600

    initial commit of script to do fun stuff with JPLs periodic orbits API

---
+++ jpl3bodyAPI.py
@@ -0,0 +1,21 @@
+
+"""
+Script to query JPL Horizons for the initial conditions of periodic 3-body orbits, their families, and other data.
+
+"""
+import requests
+import json
+import os
+
+def queryJPL():
+    """
+    Get information from the JPL Horizons periodic 3-body orbits API.
+    """
+
+
+def plotFamily():
+    """
+    Plots a family of periodic orbits.
+    """
+
+    
